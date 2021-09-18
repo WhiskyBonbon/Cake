884a83396914cece21f96b0de6527e5293c0b693
Yuuki0210 <87485667+Yuuki0210@users.noreply.github.com>
2021-09-18 12:56:34 +0900

    Update 20210917094803_create_items.rb

--- db/migrate/20210917094803_create_items.rb
+++ db/migrate/20210917094803_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.integer :gemre, null:false
+      t.integer :genre_id, null:false
       t.string :name, null:false
       t.string :image_id, null:false
       t.text :explanation, null:false
