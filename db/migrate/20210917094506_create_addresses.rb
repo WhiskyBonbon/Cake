f128885afd8e40c530ff9e3b9274cc16b2e2d091
suzuya0 <sakura240613@gmail.com>
2021-09-18 08:38:33 +0000

    [Add]genre_index_page,genre_edit_page

--- db/migrate/20210917094506_create_addresses.rb
+++ db/migrate/20210917094506_create_addresses.rb
@@ -2,9 +2,9 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
       t.integer :customer_id, null:false
-      t.strng :postal_code, null:false
-      t.strng :address, null:false
-      t.strng :name, null:false
+      t.string :postal_code, null:false
+      t.string :address, null:false
+      t.string :name, null:false
 
       t.timestamps
     end
