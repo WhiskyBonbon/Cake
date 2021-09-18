cfce39f91da1700065edf14e5733a5683fe09e75
Yuuki0210 <87485667+Yuuki0210@users.noreply.github.com>
2021-09-18 12:55:19 +0900

    Update 20210917094847_create_order_items.rb

--- db/migrate/20210917094847_create_order_items.rb
+++ db/migrate/20210917094847_create_order_items.rb
@@ -1,6 +1,8 @@
 class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
+      t.integer :order_id, null:false
+      t.integer :item_id, null:false
       t.integer :piece, null:false
       t.integer :taxed_price, null:false
       t.integer :production_status, null:false, default: 0
