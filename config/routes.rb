01555b5dce5231cda56951d6778f46352403893b
ybnr1130 <muuckk@gmail.com>
2021-09-18 02:49:59 +0000

    [Add]routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-      devise_for :managers, controllers: {
+    devise_for :managers, controllers: {
       sessions: 'admins/sessions',
       registrations: 'admins/registrations',
       passwords: 'admins/passwords'
     }
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, except: [:create, :destroy, :new]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_items, only: [:update]
   end
 
   scope module: 'customer' do
@@ -14,8 +19,22 @@ Rails.application.routes.draw do
       registrations: 'customers/registrations',
       passwords: 'customers/passwords'
     }
-    
-    
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:create, :index, :update, :destroy]
+    delete '/cart_items' => 'cart_items#all_destroy', as: 'all_destroy'
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/check' => 'orders#check', as: 'check'
+    get 'orders/thanks' => 'orders#thanks', as: 'thanks'
+    resource :customers, only: [:edit, :update]
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
+    get 'customers/delete' => 'customers#delete', as: 'delete'
+    patch 'customers/deleted' => 'customers#deleted', as: 'deleted'
+    resources :addresses, except: [:new, :show]
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
   end
+  
+  
+  
   
 end
