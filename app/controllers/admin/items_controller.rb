6861845b1e7c2b988a1e605c29c9b35c6d64c6d3
ybnr1130 <muuckk@gmail.com>
2021-09-18 11:49:23 +0000

    Add show method

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,28 @@
 class Admin::ItemsController < ApplicationController
+
+
+  def new
+    @item = Item.new
+    # @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+  
+  def show
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image_id, :explanation, :is_active, :price)
+  end
+
 end
