50a997211e94239cad7efa3470c02f44da3c01ec
ybnr1130 <muuckk@gmail.com>
2021-09-18 11:27:05 +0000

    Update to add new items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,24 @@
 class Admin::ItemsController < ApplicationController
 
   def new
+    @item = Item.new
+    # @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :imageid, :explanation, :is_active, :price)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+
+  attachment :image
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+
 end
