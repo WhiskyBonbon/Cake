792b839716c024531df38f024008055835cf0e49
ybnr1130 <muuckk@gmail.com>
2021-09-19 08:15:34 +0000

    Add cart_items index function

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,2 +1,8 @@
 class Customer::CartItemsController < ApplicationController
+  
+  def index
+    @cart_items = CartItems.where(customer_id: current_customer.id)
+    @number = 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
 
   attachment :image
   belongs_to :genre
+  has_many :cart_item
   
 end
