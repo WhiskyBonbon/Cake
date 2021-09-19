cc8fcffd9e185dd8f7504924646df2805673af78
ybnr1130 <muuckk@gmail.com>
2021-09-19 03:53:44 +0000

    Add index function and show function

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,2 +1,12 @@
 class Customer::ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+    @items = Item.page(params[:page]).per(10)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1e805825278d790a415714ee66ed950f6bde6099eb827cfaf324465a42fd93cddecc7c598681fb17db1464dd4df6c19fa99197b3f06187c6b934ca6f2c428f44'
