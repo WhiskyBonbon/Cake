ebd059d00179a925daa8245f4a9f6b3557824e90
ybnr1130 <muuckk@gmail.com>
2021-09-19 08:58:23 +0000

    Update cart_items_controller

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,8 +1,29 @@
 class Customer::CartItemsController < ApplicationController
   
   def index
-    @cart_items = CartItems.where(customer_id: current_customer.id)
-    @number = 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @number = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  end
+  
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @cart_items.each do |cartitem|
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def all_destroy
+  end
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :piece)
   end
   
 end
