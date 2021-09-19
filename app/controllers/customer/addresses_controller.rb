20fbcac464b6b5a0df4f78aeb9cb7f277e418aeb
EC2 Default User <ec2-user@ip-172-31-84-11.ec2.internal>
2021-09-19 13:58:39 +0000

    address_first_commit

--- app/controllers/customer/addresses_controller.rb
+++ app/controllers/customer/addresses_controller.rb
@@ -1,2 +1,35 @@
 class Customer::AddressesController < ApplicationController
+
+  def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def create
+    @address = Address.new(@address)
+    @address.save
+    redirect_to addresses_path
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
+
+
+
 end
