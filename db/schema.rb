8fc1e4bd27a93affe68ba8da9adcd471af050bf6
tona44 <kuryu.waku@gmail.com>
2021-09-20 04:51:05 +0000

    [Add] ジャンル検索窓

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_09_20_023552) do
 
   create_table "addresses", force: :cascade do |t|
