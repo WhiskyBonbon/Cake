562005687facf6bf9ddcad3e636cd6d06314f2eb
tona44 <kuryu.waku@gmail.com>
2021-09-21 07:38:11 +0000

    [Add] admin/顧客詳細ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,4 +3,9 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = User.page(params[:page])
   end
+  
+  def show
+    @customer = User.find(params[:id])
+  end
+  
 end
