07d4bbbacaf83f9422e22274d41115601d928f23
Yuuki0210 <kni.hose@gmail.com>
2021-09-24 11:22:04 +0000

    status_only

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,19 +1,17 @@
 class Admin::OrdersController < ApplicationController
+
   def show
     @order = Order.find(params[:id])
-    @order_items = @order.order_items
+    @order_items = OrderItem.where(order_id: params[:id])
   end
 
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
-    @order.update(order_params)
-    if @order.status == 1
-      @order.order_items.update(status: 1)
-    end
-    redirect_to :back
+    redirect_to admin_order_path
   end
 end
+
 def order_params
   params.require(:order).permit(:status)
 end
